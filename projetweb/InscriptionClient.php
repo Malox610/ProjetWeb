088d0ddd5419b9b415fb9ca2b659a7fe39a0548b
Thomas Lopez <thomaslopezpagat@gmail.com>
2022-05-28 03:39:00 +0200

    inscription html

--- projetweb/InscriptionClient.php
+++ projetweb/InscriptionClient.php
@@ -16,11 +16,7 @@
         $codepostal = $_POST["codepostal"];
         $pays = $_POST["pays"];
         $password = $_POST["mdp"];
-     
-       
-        
 
-    
     }
     //connectez-vous dans votre BDD
     //Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
@@ -32,14 +28,14 @@
         $result= mysqli_query($db_handle, $ad);
             $sport = "SELECT id_adresse FROM adresse WHERE nom_rue LIKE '$adresse'";
             $result1 = mysqli_query($db_handle, $sport);
-            if ($data = mysqli_fetch_assoc($result1)){  
+            if ($data = mysqli_fetch_assoc($result1)){
                 $idadresse = $data['id_adresse'];
 
             }
             $sql = "INSERT INTO `client` (`nom`, `prenom`, `telephone`, `email`, `password`, `num_etudiant`,`id_adresse`) VALUES ('$nom','$prenom','$telephone','$email','$password','$numEtudiant','$idadresse')";
             $result = mysqli_query($db_handle, $sql);
             echo "Inscription réussie";
-          
+
         }//end if
     //si le BDD n'existe pas
     else {
