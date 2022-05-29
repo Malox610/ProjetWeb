1a938a14bf598bd41c2d944617a565d52cc84045
Malox610 <75571264+Malox610@users.noreply.github.com>
2022-05-29 13:43:54 +0200

    paiment et session

---
+++ projetweb/deconnexion.php
@@ -0,0 +1,12 @@
+<?php
+   
+
+    // On détruit les variables de notre session
+    session_unset ();
+
+    // On détruit notre session
+    session_destroy ();
+
+    // On redirige le visiteur vers la page d'accueil
+    header ('location: index.html');
+    ?>
