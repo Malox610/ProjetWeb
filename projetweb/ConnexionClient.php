f6fbb41d5f5a52c964a2cf31cfeda600172a5fe2
Thomas Lopez <thomaslopezpagat@gmail.com>
2022-05-30 10:44:53 +0200

    Deconnexion

--- projetweb/ConnexionClient.php
+++ projetweb/ConnexionClient.php
@@ -10,7 +10,7 @@
     }
     //connectez-vous dans votre BDD
     //Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
-    $db_handle = mysqli_connect('localhost', 'root', '' );
+    $db_handle = mysqli_connect('localhost', 'root', 'root' );
     $db_found = mysqli_select_db($db_handle, $database);
     //si le BDD existe, faire le traitement
     if ($db_found) {
